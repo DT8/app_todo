596f970a8101601502b2cd6ca76b6d77d5616b61
AlexSchipunov <shipunov8@gmail.com>
2021-07-04 17:49:16 +0300

    add main struct of app

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	todo "github.com/DT8/app_todo"
+	"github.com/DT8/app_todo/pkg/handler"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+	srv := new(todo.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("server error: %v", err)
+	}
 }
